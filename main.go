ad8f46901537cd5fd77c622c27d0fd842f3aab58
edwin628 <1143934303@qq.com>
2024-05-13 05:24:23 +0200

    add curl test, context part finished

--- main.go
+++ main.go
@@ -12,11 +12,22 @@ func main() {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	// expect /hello?name=geektutu
+	// curl "http://localhost:9999/hello?name=geektutu"
 	r.GET("/hello", func(c *gee.Context) {
 		c.String(http.StatusOK, "hello %s, you are at %s\n", c.Request.URL.Query().Get("name"), c.Path)
 	})
 
+	r.GET("/hello/:name", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you are at %s\n", c.Params["name"], c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(gee.DataStruct{
+			"filepath": c.Params["filepath"],
+		})
+	})
+
+	// curl "http://localhost:9999/login" -X POST -d 'username=geektutu&password=1234'
 	r.POST("/login", func(c *gee.Context) {
 		c.JSON(gee.DataStruct{
 			"username": c.Request.FormValue("username"),
